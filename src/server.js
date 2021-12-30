4326b0dbdde457ad3565d9671061b13af9e2dc67
최성준 <db970406@naver.com>
2021-12-30 14:50:56 +0900

    setup2

--- src/server.js
+++ src/server.js
@@ -3,7 +3,15 @@ import express from "express"
 
 const app = express()
 
-console.log("hello")
+app.set("view engine", "pug")
+app.set("views", process.cwd() + "/src/views")
+
+app.use("/public", express.static(__dirname + "/public"))
+
+const home = (req, res) => res.render("home")
+app.get("/", home)
+app.get("/*", (req, res) => res.redirect("/"))
+// 어떤 주소로 가든 home으로 보내버린다.
 
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server on http://localhost:${PORT}`))
