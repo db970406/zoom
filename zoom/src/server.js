449568e5aa62349f762d23fca4dff110d540d1ef
최성준 <db970406@naver.com>
2022-01-02 21:13:13 +0900

    Connection with front & back by using SocketIO

---
+++ zoom/src/server.js
@@ -0,0 +1,28 @@
+import "dotenv/config"
+import express from "express"
+import http from "http"
+//import WebSocket from 'ws'
+import SocketIO from "socket.io"
+
+const app = express()
+
+app.set("view engine", "pug")
+app.set("views", process.cwd() + "/src/views")
+
+app.use("/public", express.static(__dirname + "/public"))
+
+const home = (req, res) => res.render("home")
+app.get("/", home)
+app.get("/*", (req, res) => res.redirect("/"))
+// 어떤 주소로 가든 home으로 보내버린다.
+
+const PORT = process.env.PORT
+
+const httpServer = http.createServer(app)
+const wsServer = SocketIO(httpServer)
+
+wsServer.on("connection", (socket) => {
+    console.log(socket)
+})
+
+httpServer.listen(PORT, () => console.log(`Server on http://localhost:${PORT}`))
